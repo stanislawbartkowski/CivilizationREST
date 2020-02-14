b733c9b43f1787b19d31df87335960def40752b3
stanislawbartkowski@gmail.com <leszek123>
2020-02-15 00:17:51 +0100

    RestService

--- mvndeploy.sh
+++ mvndeploy.sh
@@ -1,1 +1,2 @@
 mvn install:install-file -Dfile=lib/CivilizationEngine.jar  -DgroupId=com.civlizationengine  -DartifactId=CivilizationEngine  -Dpackaging=jar -Dversion=1.0
+mvn install:install-file -Dfile=lib/RestService.jar  -DgroupId=com.civlizationengine  -DartifactId=RestService  -Dpackaging=jar -Dversion=1.0
