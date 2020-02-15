87e3cd6961b49a9568d7926119ae868e3d78bc99
sb <sb@thinkde.sb.com>
2020-02-15 21:22:44 +0100

    main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-. ./rest.rc
-java -cp target/CivRestHTTP-1.0-SNAPSHOT-jar-with-dependencies.jar  CivHttpServer ${PORT} ${REDISHOST} ${REDISPORT}
+java -cp CivRestHTTP-1.0-SNAPSHOT-jar-with-dependencies.jar  CivHttpServer ${PORT} ${REDISHOST} ${REDISPORT}
